eadb8b3b86dd018ad87d285008629eebb1afb9b7
Ben Alonzo <alonzo.b@northeastern.edu>
2023-09-21 13:47:09 -0400

    reset Loader class to defaults to fix buggy code

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,18 +1,8 @@
-import { Html, useProgress } from '@react-three/drei';
+import React from 'react'
 
 const Loader = () => {
   return (
-    <Html>
-      <span className="canvas-load"></span>
-      <p
-        style={{
-          fontSize: 14,
-          color: '#f1f1f1',
-          fontWeight: 800,
-          marginTop: 40
-        }}
-      >{progress.toFixed(2)}%</p>
-    </Html>
+    <div>Loader</div>
   )
 }
 
