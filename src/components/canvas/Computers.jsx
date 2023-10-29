e47db416980e7038961e9d1218304add1e3f742f
Ben Alonzo <alonzo.b@northeastern.edu>
2023-10-29 18:54:09 -0400

    working on navbar infinite scrolling and mobile resizing

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ( { isMobile} ) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
@@ -12,21 +12,43 @@ const Computers = () => {
       <hemisphereLight intensity={5} groundColor="black" />
       <pointLight intensity={10} position={[5, -1.2, 0]}/>
       <pointLight intensity={10} position={[-5, -1.2, 0]}/>
-      <primitive object={computer.scene}
-      scale={4}
-      position={[0, -1, 0]}
-      rotation={[0.0, -3.15, 0.0]}
+      <primitive 
+        object={computer.scene}
+        scale={isMobile ? 2.5 : 2.75}
+        position={[0, -1.5, 0]}
+        rotation={[0.0, -3.15, 0.0]}
       /> 
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(
+    ('max-width: 500px'));
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change',
+    handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change',
+      handleMediaQueryChange);
+    }
+
+  }, [])
   return (
     <Canvas
       frameLoop="demand"
       shadows
-      camera={{position: [10, 20, 5], fov: 30}}
+      camera={{position: [10, 20, 5], fov: 25}}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -35,7 +57,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={(Math.PI / 2)}
           minPolarAngle={(Math.PI / 2)}
         />
-        <Computers />
+        <Computers isMobile={isMobile}/>
       </Suspense>
       <Preload all />
     </Canvas>
